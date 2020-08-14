b06b30c8cb0f258be2264991822dfe12f5499d5f
flacy <me@flacy.me>
2020-08-14 05:18:10 +0300

    created setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='aiohttp-rate-limiter',
+    version='0.0.2',
+    author='Flacy',
+    author_email='me@flacy.me',
+    description='A library for control and limiting requests with aiohttp framework',
+    url='https://github.com/Flacy/aiohttp-rate-limiter',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha'
+    ],
+    python_requires='>=3.7'
+)
