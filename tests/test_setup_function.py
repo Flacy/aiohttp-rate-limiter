9d458b27807f8da399481f87e6f8adcf0bc5dadc
flacy <me@flacy.me>
2020-08-15 03:07:27 +0300

    added pytests

---
+++ tests/test_setup_function.py
@@ -0,0 +1,19 @@
+import pytest
+from aiohttp import web
+from aiohttp_rate_limiter import setup as setup_rate
+
+
+@pytest.fixture
+def cli(loop, aiohttp_client):
+    async def handler(request):
+        return web.HTTPNotFound()
+    app = web.Application()
+    setup_rate(app, error_handler=handler, method='fixed_window', max_requests=1, interval=100)
+    return loop.run_until_complete(aiohttp_client(app))
+
+
+async def test_error_handler(cli):
+    resp = await cli.get('/')
+    assert resp.status == 404
+    resp = await cli.get('/')
+    assert resp.status == 404
