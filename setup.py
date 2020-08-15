baaed498b2a0015a068a99b3125c57e1cd5f4b31
flacy <me@flacy.me>
2020-08-15 03:46:39 +0300

    changed require version of aiohttp to avoid using unstable release

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
         "Operating System :: OS Independent",
         'Development Status :: 3 - Alpha'
     ],
-    install_requires=['aiohttp>=3.5.2'],
+    install_requires=['aiohttp<=4.0.0'],
     python_requires='>=3.7'
 )
