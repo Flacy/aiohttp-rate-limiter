29d1704ca5a927975f1955128728caf3171ffd0b
flacy <me@flacy.me>
2020-08-16 10:19:32 +0300

    changed version and added tests for new method

---
+++ tests/test_sliding_log.py
@@ -0,0 +1,17 @@
+import pytest
+from aiohttp import web
+from aiohttp_rate_limiter import setup as setup_rate
+
+
+@pytest.fixture
+def cli(loop, aiohttp_client):
+    app = web.Application()
+    setup_rate(app, method='sliding_log', max_requests=1)
+    return loop.run_until_complete(aiohttp_client(app))
+
+
+async def test_block(cli):
+    resp = await cli.get('/')
+    assert resp.status == 404
+    resp = await cli.get('/')
+    assert resp.status == 429
